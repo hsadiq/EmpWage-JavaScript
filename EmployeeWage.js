f935038e6d92d764c80a5eb480621ea3393279bb
hsadiq <ksadik320@gmail.com>
2023-06-02 11:40:22 +0530

    Added Calculating WageCondition

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -2,19 +2,24 @@ console.log("Welcome To EmployeeWage!!");
 
 const wagePerHour = 20;
 const fullDayHours = 8;
+const partTimeHours = 4;
 
 let employeeAttendance;
-let check = Math.floor(Math.random() * 2);
+let check = Math.floor(Math.random() * 3);
 
-if (check !== 0) {
-    employeeAttendance = "Present";
-} else {
+if (check === 0) {
     employeeAttendance = "Absent";
+} else if (check === 1) {
+    employeeAttendance = "Part-Time";
+} else {
+    employeeAttendance = "Full-Time";
 }
 
 let employeeWage = 0;
 
-if (employeeAttendance === "Present") {
+if (employeeAttendance === "Part-Time") {
+    employeeWage = wagePerHour * partTimeHours;
+} else if (employeeAttendance === "Full-Time") {
     employeeWage = wagePerHour * fullDayHours;
 }
 
