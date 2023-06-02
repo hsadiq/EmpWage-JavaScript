1b66fc41589279d860f65099347dbc91a5b41ebd
hsadiq <ksadik320@gmail.com>
2023-06-02 09:47:04 +0530

    Added Employee Wage Problem Using JavaScript

---
+++ EmployeeWage.js
@@ -0,0 +1,12 @@
+//UC-1 Check Employee is Present!
+
+let Employee_Attandance;
+
+let Check = Math.random()*2;
+
+if (Check != 0){
+    Employee_Attandance = "Present";
+}else
+    Employee_Attandance = "Absent";
+
+console.log(Employee_Attandance);
