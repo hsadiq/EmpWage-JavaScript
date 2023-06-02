7c579848259d8fb012ee32c075e5d70dc859c90c
hsadiq <ksadik320@gmail.com>
2023-06-02 11:48:32 +0530

    Added Calculate Wage For a month In EmployeeWage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -3,34 +3,40 @@ console.log("Welcome To EmployeeWage!!");
 const wagePerHour = 20;
 const fullDayHours = 8;
 const partTimeHours = 4;
+const workingDaysPerMonth = 20;
 
-let employeeAttendance;
-let check = Math.floor(Math.random() * 3);
+let totalWage = 0;
 
-switch (check) {
-    case 0:
-        employeeAttendance = "Absent";
-        break;
-    case 1:
-        employeeAttendance = "Part-Time";
-        break;
-    case 2:
-        employeeAttendance = "Full-Time";
-        break;
-    default:
-        employeeAttendance = "Invalid";
+for (let day = 1; day <= workingDaysPerMonth; day++) {
+    let employeeAttendance;
+    let check = Math.floor(Math.random() * 3);
+
+    switch (check) {
+        case 0:
+            employeeAttendance = "Absent";
+            break;
+        case 1:
+            employeeAttendance = "Part-Time";
+            break;
+        case 2:
+            employeeAttendance = "Full-Time";
+            break;
+        default:
+            employeeAttendance = "Invalid";
+    }
+
+    let employeeWage = 0;
+
+    switch (employeeAttendance) {
+        case "Part-Time":
+            employeeWage = wagePerHour * partTimeHours;
+            break;
+        case "Full-Time":
+            employeeWage = wagePerHour * fullDayHours;
+            break;
+    }
+
+    totalWage += employeeWage;
 }
 
-let employeeWage = 0;
-
-switch (employeeAttendance) {
-    case "Part-Time":
-        employeeWage = wagePerHour * partTimeHours;
-        break;
-    case "Full-Time":
-        employeeWage = wagePerHour * fullDayHours;
-        break;
-}
-
-console.log("Employee Attendance: " + employeeAttendance);
-console.log("Employee Wage: " + employeeWage);
+console.log("Total Wage for the Month: " + totalWage);
